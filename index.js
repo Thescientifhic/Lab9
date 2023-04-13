ce7ead451ffd7970e708edd83ddd8abadd80a7ae
Thescientifhic <sebasartu2110gmail.com>
2023-04-12 19:50:59 -0500

    Publicación

---
+++ index.js
@@ -0,0 +1,40 @@
+const openModal = document.querySelector('.btn1');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal_close');
+const boton = document.getElementById("button")
+const textarea = document.getElementById("comentario")
+
+openModal.addEventListener('click', ()=>{
+    modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', ()=>{
+    modal.classList.remove('modal--show');
+});
+
+function activar() {
+    var value = textarea.value
+    if ( value === "") {
+        boton.classList.remove("activaBoton")
+        boton.classList.add("desactivaBoton")
+        boton.disabled = true;
+    } else {
+        boton.classList.remove("desactivaBoton")
+        boton.classList.add("activaBoton")
+        boton.disabled = false;
+    }
+}
+
+function subirPublicacion() {
+    var text = textarea.value
+    var comment = `
+    <div class="comentarios">
+        <div class="perfil-s">
+            <div><img class="img-2" src="./mujer.png" alt=""></div>
+        <div class="name"><label for="img">Andrea Andrade</label></div>
+    </div>
+        <p class="publiComment">${text}</p>
+    </div>
+    `
+    contenedorComentarios.innerHTML += comment
+}
